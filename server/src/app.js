9d8ef328495b397cd8d0cff56493ca29b4ec0be8
seifelden66 <seifelden66@gmail.com>
2024-04-21 00:00:36 +0200

    made products simple crud api

--- server/src/app.js
+++ server/src/app.js
@@ -1,56 +1,36 @@
-//app.js
 const fs = require("fs");
 const path = require("path");
 const mongoose = require('mongoose');
 const express = require("express");
-<<<<<<< HEAD
 const cors = require("cors");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const requestIp = require("request-ip");
 const api = require("./routes/api");
-<<<<<<< HEAD
-=======
-const productRouter = require('./routes/products/products.router'); // Correct import
-const ObjectId = mongoose.Types.ObjectId;
-=======
-const morgan = require("morgan");
-const api = require("./routes/api");
 const productRouter = require('./routes/products/products.router'); 
 const categoriesRouter = require('./routes/cetgories/categories.route')
->>>>>>> 4050770 (created categories apis)
 
 const ObjectId = mongoose.Types.ObjectId;
 const app = express();
->>>>>>> 9db4ca1 (made products simple crud api)
 
-const app = express();
 app.use(requestIp.mw());
 app.use(helmet());
 app.use(cors({ origin: "http://localhost:4200" }));
+
 const accessLogStream = fs.createWriteStream("access.log", { flags: "a" });
 app.use(morgan("combined", { stream: accessLogStream }));
-<<<<<<< HEAD
+
 app.use(express.json());
-// error handler midddleware
-app.use((err, req, res, next) => {
-  throw Error(err.stack);
-});
-=======
-//middleware
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({extended:false}));
 
 app.use("/api/products", productRouter);
-<<<<<<< HEAD
->>>>>>> 9db4ca1 (made products simple crud api)
-=======
 app.use("/api/categories", categoriesRouter);
->>>>>>> 4050770 (created categories apis)
 
 app.use(express.static(path.join(__dirname, "..", "public", "browser")));
 app.use("/v1", api);
+
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "browser", "index.html"));
 });
+
 module.exports = app;
--- server/src/models/products/product.model.js
+++ server/src/models/products/product.model.js
@@ -1,6 +1,10 @@
+<<<<<<< HEAD
 //models/products/product.model.js
 const mongoose = require('mongoose')
 const Category = require('../categories/categories.model'); 
+=======
+const mongoose = require('mongoose')
+>>>>>>> d37ef13 (made products simple crud api)
 const productSchema = mongoose.Schema(
     {
         name: {
@@ -9,12 +13,18 @@ const productSchema = mongoose.Schema(
         },
         quantity: {
             type: Number,
+<<<<<<< HEAD
             default: 1
+=======
+            required: true,
+            default: 0
+>>>>>>> d37ef13 (made products simple crud api)
         },
         price: {
             type: Number,
             required: true,
         },
+<<<<<<< HEAD
         images: [{
             type: String,
             required: false
@@ -24,6 +34,12 @@ const productSchema = mongoose.Schema(
             ref: 'Category',
             required: false
         }/*  */
+=======
+        image: {
+            type: String,
+            required: false
+        }
+>>>>>>> d37ef13 (made products simple crud api)
     },
     {
         timestamps:true
