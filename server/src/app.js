14a57372927e8df3655328f0c75b4c62f1c9dc01
seifelden66 <seifelden66@gmail.com>
2024-04-21 00:00:36 +0200

    made products simple crud api

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,23 @@
 const fs = require("fs");
 const path = require("path");
+const mongoose = require('mongoose');
 const express = require("express");
 // const cors = require("cors"); // TODO
 const morgan = require("morgan");
 // const helmet = require("helmet"); // TODO
 const api = require("./routes/api");
+const productRouter = require('./routes/products/products.router'); // Correct import
+const ObjectId = mongoose.Types.ObjectId;
+
 const app = express();
 
 const accessLogStream = fs.createWriteStream("access.log", { flags: "a" });
 app.use(morgan("combined", { stream: accessLogStream }));
-app.use(express.json());
+//middleware
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.use("/api/products", productRouter);
 
 app.use("/v1", api);
 
