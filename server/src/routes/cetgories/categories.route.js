31ac199a7b200af8ceb090e1665ef351cd23997b
seifelden66 <seifelden66@gmail.com>
2024-04-22 13:46:43 +0200

    get category endpoint modified

--- server/src/routes/cetgories/categories.route.js
+++ server/src/routes/cetgories/categories.route.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const {getCategories, updateCategory, deleteCategory, postCategory} = require('../../controllers/categories.controller')
+const {getCategories, updateCategory, deleteCategory, postCategory, getCategory} = require('../../controllers/categories.controller')
 
 router.get('/', getCategories)
-router.get('/:id', getCategories)
+router.get('/:id', getCategory)
 router.post('/', postCategory)
 router.put('/:id',updateCategory)
 router.delete('/:id', deleteCategory)
