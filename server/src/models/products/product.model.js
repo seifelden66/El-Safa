f11028299b9ecbae15cce97722e69698ee7a4979
seifelden66 <seifelden66@gmail.com>
2024-04-23 02:27:09 +0200

    fixed errors

--- server/src/models/products/product.model.js
+++ server/src/models/products/product.model.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 //models/products/product.model.js
 const mongoose = require('mongoose')
 const Category = require('../categories/categories.model'); 
-=======
-const mongoose = require('mongoose')
->>>>>>> d37ef13 (made products simple crud api)
 const productSchema = mongoose.Schema(
     {
         name: {
@@ -13,18 +9,12 @@ const productSchema = mongoose.Schema(
         },
         quantity: {
             type: Number,
-<<<<<<< HEAD
             default: 1
-=======
-            required: true,
-            default: 0
->>>>>>> d37ef13 (made products simple crud api)
         },
         price: {
             type: Number,
             required: true,
         },
-<<<<<<< HEAD
         images: [{
             type: String,
             required: false
@@ -33,13 +23,7 @@ const productSchema = mongoose.Schema(
             type: mongoose.Schema.Types.ObjectId,
             ref: 'Category',
             required: false
-        }/*  */
-=======
-        image: {
-            type: String,
-            required: false
         }
->>>>>>> d37ef13 (made products simple crud api)
     },
     {
         timestamps:true
