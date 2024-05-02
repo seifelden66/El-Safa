01f1a1bfcd6862228dcfc932b87209d025f2a7d3
kareem0913 <kareem.345@yahoo.com>
2024-05-02 06:45:46 +0300

    continue integrate orders track function between backend and client, and implement add new admin to our dashboard

--- client/src/app/admin/single-order/single-order.component.ts
+++ client/src/app/admin/single-order/single-order.component.ts
@@ -18,6 +18,10 @@ export class SingleOrderComponent {
   adminToken!: any;
   user!: any;
   orders!: any;
+  orderStatus!: string;
+  orderMessage!: string;
+  modelStatus: boolean = false;
+
   constructor(
     private ActivatedRoute: ActivatedRoute,
     private http: HttpClient,
@@ -41,6 +45,7 @@ export class SingleOrderComponent {
         (res: any) => {
           this.user = res.user;
           this.orders = res.order;
+          this.orderStatus = res.order.order_status;
         },
         (error) => {
           console.log(error.error);
@@ -49,10 +54,75 @@ export class SingleOrderComponent {
   }
 
   confirmOrder(id: string) {
-    console.log(id);
+    this.http
+      .patch(
+        "http://localhost:8000/v1/admin/confirm-order",
+        { id: this.orderId },
+        {
+          headers: {
+            Authorization: `Bearer ${this.adminToken}`,
+          },
+        }
+      )
+      .subscribe(
+        (res: any) => {
+          this.orderStatus = res.orderStatus;
+          this.modelStatus = true;
+          this.orderMessage = res.message;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
   }
-  
-  dispatchOrder(id: string) {}
 
-  deliverOrder(id: string) {}
+  dispatchOrder(id: string) {
+    this.http
+      .patch(
+        "http://localhost:8000/v1/admin/dispatch-order",
+        { id: this.orderId },
+        {
+          headers: {
+            Authorization: `Bearer ${this.adminToken}`,
+          },
+        }
+      )
+      .subscribe(
+        (res: any) => {
+          this.orderStatus = res.orderStatus;
+          this.modelStatus = true;
+          this.orderMessage = res.message;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
+
+  deliverOrder(id: string) {
+    this.http
+      .patch(
+        "http://localhost:8000/v1/admin/deliver-order",
+        { id: this.orderId },
+        {
+          headers: {
+            Authorization: `Bearer ${this.adminToken}`,
+          },
+        }
+      )
+      .subscribe(
+        (res: any) => {
+          this.orderStatus = res.orderStatus;
+          this.modelStatus = true;
+          this.orderMessage = res.message;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
+
+  hideModel() {
+    this.modelStatus = false;
+  }
 }
